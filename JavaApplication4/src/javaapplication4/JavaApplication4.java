82d74d696f0366e69069309ed821a74587529522
Hugo Carreira <hugo.colombo23@gmail.com>
2017-08-14 12:29:46 -0300

    Update logic from ex4

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -10,14 +10,42 @@ import java.util.Scanner;
 public class JavaApplication4 {
 
     public static void main(String[] args) {        
-        int maior = 0, menor = 0, def = 0; 
-        float media = 0, count = 0;
+        int maior = 0, menor = 0, def = 0, cont = 0, count = 0; 
+        double media = 0;
         Scanner sc = new Scanner(System.in); 
         
         System.out.print("Digite um numero correspondente à definição: ");
-        def = (Integer.parseInt(sc.nextLine()));
+        def = (Integer.parseInt(sc.nextLine()));                
+
+        maior = def;
+        menor = def;
+        while(def != -1) {
+            count = count + def;
+            cont = cont + 1;
+            
+            if(def > maior) {
+                maior = def;
+            }
+            if(def < menor) {
+                menor = def;
+            }
+            
+            System.out.print("Digite um valor: ");
+            def = Integer.parseInt(sc.nextLine());
+        }    
         
-        if(def != -1) {                   
+        media = (double) count / cont;
+        System.out.println("Media = " + media);
+        System.out.println("Maior valor = " + maior);
+        System.out.println("Menor valor = " + menor);
+    }
+    
+    public static void OldLogic() {
+        int maior = 0, menor = 0, def = 0, count = 0; 
+        double media = 0;
+        Scanner sc = new Scanner(System.in); 
+
+        if(def != -1) {
             int cj[] = new int[def];
         
             for(int i = 0; i < cj.length; i++){
@@ -32,9 +60,5 @@ public class JavaApplication4 {
             }           
             media = count / def;
         }
-        
-        System.out.println("Media = " + media);
-        System.out.println("Maior valor = " + maior);
-        System.out.println("Menor valor = " + menor);
-    }   
+    }
 }
